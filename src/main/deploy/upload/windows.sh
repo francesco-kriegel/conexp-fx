129a8c0b0ba80122d306520a23f7f9879b1ea5ca
Francesco Kriegel <francesco-kriegel@users.noreply.github.com>
2018-10-27 08:09:05 +0200

    Minor change in project's deployment configuration

--- src/main/deploy/upload/windows.sh
+++ src/main/deploy/upload/windows.sh
@@ -3,5 +3,5 @@ echo "Uploading Windows Bundles..."
 version=$(printf 'VERSION=${project.version}' | mvn help:evaluate | grep '^VERSION=' | sed 's/^VERSION=//g')
 date=$(date +%Y-%m-%d)
 
-ssh francesco@tcs.inf.tu-dresden.de "mkdir -p public_html/conexp-fx/download/$version/$date"
+ssh francesco@tcs.inf.tu-dresden.de "mkdir -p public_html; mkdir -p public_html/conexp-fx; mkdir -p public_html/conexp-fx/download; mkdir -p public_html/conexp-fx/download/$version; mkdir -p public_html/conexp-fx/download/$version/$date"
 scp -r target/jfx/native/*.msi francesco@tcs.inf.tu-dresden.de:public_html/conexp-fx/download/$version/$date/
